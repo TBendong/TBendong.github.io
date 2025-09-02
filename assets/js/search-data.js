aba6a5adc30b64349219f5a68ddf1485ae16b114
TBendong <59835927+TBendong@users.noreply.github.com>
2025-09-02 01:19:20 +0000

    Deploying to gh-pages from @ TBendong/TBendong.github.io@fdc0bfbaeb6354af2d6adf202002f124c8117485 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/publications/";
+            window.location.href = "/Publications/";
           },
         },{id: "nav-services",
           title: "Services",
